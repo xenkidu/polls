0da0daa255bbd22e540105e135437e23fbbd81a1
xenkidu <aburton85@live.com>
2020-07-10 13:59:17 -0500

    [app: polls; feat: view/template] Created the template for the index_view.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,23 @@
 from django.http import HttpResponse
 
+from .models import Question
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index_view(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail_view(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results_view(request, question_id):
+    response = f"You're looking at the results of question {question_id}"
+    return HttpResponse(response)
+
+
+def vote_view(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}")
+
+
