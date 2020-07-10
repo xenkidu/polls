33e949d30a9e5c010be11a78a7642445976411c0
xenkidu <aburton85@live.com>
2020-07-10 15:20:18 -0500

    [app: polls; feat: template,routes] Refactored views to extend the base.html template. Started the Form for vote submissions.

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index_view, name='index'),
     path('<int:question_id>/', views.detail_view, name='detail'),
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,21 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render, get_object_or_404
+
 from .models import Question
+
 
 def index_view(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail_view(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results_view(request, question_id):
