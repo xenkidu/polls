63394eb020491d0c6042082712c9da20c2e7fb71
xenkidu <aburton85@live.com>
2020-07-13 15:46:28 -0500

    [app: accounts; feat: users] User registration form created and working, creating users.

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('', views.index_view, name='index'),
+    path('', include('django.contrib.auth.urls')),
+    path('', views.index_view, name="register"),
 ]
--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
+from django.contrib import messages
 
+from .forms import UserRegisterForm
 
 def index_view(request):
-    context = {}
-    return render(request, 'accounts/index.html', context)
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('polls:index')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'accounts/register.html', {'form': form})
