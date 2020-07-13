865e8056a0cf12bb96d6a9f605c99f0b527483dc
xenkidu <aburton85@live.com>
2020-07-13 14:33:46 -0500

    [app: accounts; feat: users] Created templates and wired up basic view for index.

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+def index_view(request):
+    context = {}
+    return render(request, 'accounts/index.html', context)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # custom apps
     'polls.apps.PollsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
