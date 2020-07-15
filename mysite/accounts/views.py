fe1ac843b97639684da73ce05778accc60ff3984
xenkidu <aburton85@live.com>
2020-07-15 13:04:50 -0500

    [accounts; update profile] Added functionality to update user profile.

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, ProfileUpdateForm, UserUpdateForm
 
 def register(request):
     if request.method == 'POST':
@@ -28,3 +28,21 @@ class logout_view(views.LogoutView):
 @method_decorator(login_required, name='dispatch')
 class profile_view(views.TemplateView):
     template_name = 'accounts/profile.html'
+
+    def get(self, request, *args, **kwargs):
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+        return render(request, self.template_name, {'u_form': u_form,'p_form': p_form})
+
+    def post(self, request, *args, **kwargs):
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            # <process form cleaned data>
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Account Updated!')
+            return redirect('accounts:profile')
+
+        return render(request, self.template_name, {'u_form': u_form, 'p_form': p_form})
