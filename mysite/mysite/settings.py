cb077551bcc30ae6fcc3e70817783a527a999ca1
xenkidu <aburton85@live.com>
2020-08-25 16:34:57 -0500

    Added Heroku settings.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -132,3 +132,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'polls:home'
 
 LOGIN_URL = 'accounts:login'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
